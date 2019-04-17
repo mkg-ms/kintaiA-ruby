b41c30bdf51077d7245bd42148e0b84868a0b81d
mkg-ms <ka11270105@gmail.com>
2019-04-17 04:49:46 +0000

    Finish basic information

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ validates :email, presence: true, length: { maximum: 100 },
                   uniqueness: {case_sensitive: false }
 has_secure_password
 validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+validates :department, length: { in: 3..50 }, allow_blank: true
 
 end
 
