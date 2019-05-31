a1b30c6699160f6507d2a19053ee372952fcef76
mkg-ms <ka11270105@gmail.com>
2019-05-31 01:16:31 +0000

    Finish Additional functions

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,18 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   validates :worked_on, presence: true
-  validate :finished_at, presence: true
-  validate :finished_at, presence: true
+  validate :started_at_must_exist
+  validate :finished_at_cannot_be_faster_than_started_at
+
+  def started_at_must_exist
+    if started_at.blank? && finished_at.present?
+      errors.add("出勤時間がありません")
+    end
+  end
+  
+  def finished_at_cannot_be_faster_than_started_at 
+    if started_at.present? && finished_at.present? && started_at > finished_at
+      errors.add("出勤時間よりも後の時刻を登録してください")
+    end
+  end
 end
