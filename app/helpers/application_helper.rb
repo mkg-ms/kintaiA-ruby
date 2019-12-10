1b46afe8666448ece9c73c955ce6e328f8693677
mkg-ms <ka11270105@gmail.com>
2019-12-10 07:10:21 +0000

    heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,4 @@
 module ApplicationHelper
-  
-  
-
   # ページごとにタイトルを返す。
   def full_title(page_title = "") # メソッド名と引数の定義
     base_title = "勤怠管理システム" # 変数に代入
@@ -11,5 +8,4 @@ module ApplicationHelper
       page_title + " | " + base_title # 文字列を結合して返す。
     end
   end
-
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,8 +2,7 @@ class Attendance < ApplicationRecord
   belongs_to :user
   validates :worked_on, presence: true
   validate :must_choice_superior, on: :update
-  
-　# 勤怠編集にて指示者を確認しなければいけない。
+  # 勤怠編集にて指示者を確認しなければいけない。
   def must_choice_superior
     if started_at_2.present? && finished_at_2.present? && started_at_2 < finished_at_2 && superior_selection.nil?
       errors.add(:superior_selection, "を選択して下さい。")
@@ -15,5 +14,5 @@ class Attendance < ApplicationRecord
     where(attendances: {worked_on: Date.current, finished_at: nil}).
     where.not(attendances: {started_at: nil})
   }
-
+  
 end
